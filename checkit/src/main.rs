638cbe0f638ecc4507e0155524b7da7f5103bd62
Terrell-byte <danieltsutton@outlook.com>
2023-01-20 13:44:57 +0100

    added arrow icons instead on normal Egui buttons.

--- checkit/src/main.rs
+++ checkit/src/main.rs
@@ -6,18 +6,20 @@ use chrono::{Datelike, Local, DateTime, Duration};
 //move all of Init view into a separate file
 struct InitView {
     date_backdrop: RetainedImage,
+    arrow_left_icon: RetainedImage,
+    arrow_right_icon: RetainedImage,
     date: DateTime<Local>,
 }
 
 impl InitView {
     fn new() -> Self {
         //load images
-        let date_backdrop = RetainedImage::from_image_bytes(
-            "date_backdrop.png",
-            include_bytes!("date_backdrop.png"),
-        ).unwrap();
+        let date_backdrop = RetainedImage::from_image_bytes("date_backdrop.png", include_bytes!("date_backdrop.png")).unwrap();
+        let arrow_left_icon = RetainedImage::from_image_bytes("arrow_left.png", include_bytes!("arrow_left.png")).unwrap();
+        let arrow_right_icon = RetainedImage::from_image_bytes("arrow_right.png", include_bytes!("arrow_right.png")).unwrap();
+
         let date = Local::now();
-        Self { date_backdrop, date,}
+        Self { date_backdrop, date, arrow_left_icon, arrow_right_icon}
     }
 
     //make this into a serparate class
@@ -67,15 +69,28 @@ impl InitView {
     fn render_date(&mut self, ui: &mut eframe::egui::Ui) {
         let date = self.format_date_string();
         ui.vertical_centered(|ui|{
-            ui.add_space(30.0);
+            ui.add_space(50.0);
             ui.horizontal(|ui|{
                 ui.add_space(50.0);
                 ui.colored_label(egui::Color32::from_rgb(255, 255, 255), date);
-                ui.add_space(30.0);
-                if ui.button("prev").clicked() {
+            });
+        });
+
+    }
+
+    fn render_date_arrow(&mut self, ui: &mut eframe::egui::Ui, ctx: &egui::Context) {
+        let prev = egui::ImageButton::new(self.arrow_left_icon.texture_id(ctx), egui::vec2(7.0, 12.0));
+        let next = egui::ImageButton::new(self.arrow_right_icon.texture_id(ctx), egui::vec2(8.0, 12.0));
+
+        ui.vertical_centered(|ui|{
+            ui.add_space(50.0);
+            ui.horizontal(|ui|{
+                ui.add_space(300.0);
+                if ui.add(prev.frame(false)).clicked() {
                     self.date = self.date - Duration::days(1);
                 }
-                if ui.button("next").clicked() {
+                ui.add_space(15.0);
+                if ui.add(next.frame(false)).clicked() {
                     self.date = self.date + Duration::days(1);
                 }
             });
@@ -102,19 +117,27 @@ impl eframe::App for InitView {
             //load background
             self.render_date_backdrop(ui);
         });
-        //layer 1
+
+
+        //date layers
         egui::CentralPanel::default()
         .frame(frame)
         .show(ctx, |ui| {
             //load UI elements
             self.render_date(ui);
         });
+        egui::CentralPanel::default()
+        .frame(frame)
+        .show(ctx, |ui| {
+            //load UI elements
+            self.render_date_arrow(ui, ctx);
+        });
     }
 }
 
 fn main() {
     let options = eframe::NativeOptions {
-        initial_window_size: Some(egui::vec2(386.0, 636.0)),
+        initial_window_size: Some(egui::vec2(386.0, 685.0)),
         resizable: false,
         ..Default::default()
     };
