9f4084bf5bcc4c342df9f19e28ab4dc646123733
Terrell-byte <danieltsutton@outlook.com>
2023-01-31 14:25:53 +0100

    creates a database but cant get it to create a table on init

--- checkit/createtable.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXIST `tasks` (
-  `id` varchar(255) NOT NULL,
-  `title` varchar(255) DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  `isCompleted` BIT(2) DEFAULT 0,
-  PRIMARY KEY (`id`)
-)
---
+++ checkit/initDB.sql
@@ -0,0 +1,6 @@
+CREATE TABLE checkitDB.tasks (
+    id VARCHAR(255) NOT NULL,
+    title VARCHAR(32) NOT NULL,
+    description TEXT NOT NULL,
+    PRIMARY KEY (id)
+);
