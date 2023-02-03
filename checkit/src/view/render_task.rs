ea5f5514f1d51e4547007a170d4a1a6c1bb4d5eb
Terrell-byte <danieltsutton@outlook.com>
2023-02-03 21:51:52 +0100

    .

--- checkit/src/view/render_task.rs
+++ checkit/src/view/render_task.rs
@@ -1,22 +1,6 @@
 use eframe::egui::{self};
 use crate::view::init_view::*;
 
-pub fn render_task(view: &mut View, ctx: &egui::Context) {
-
-    let frame = egui::containers::Frame {
-        fill: egui::Color32::from_rgba_premultiplied(0, 0, 0, 0),
-        ..Default::default()
-    };
-    egui::CentralPanel::default()
-    .frame(frame)
-    .show(ctx, |ui| {
-        ui.vertical_centered(|ui|{
-            ui.add_space(260.0);
-
-            //create a iterator for the tasks that is loaded from the datebase and then render them, and give them a 18.0 space between them
-            //make sure that all of this is in a scroll area
-            view.task_background.show_size(ui, egui::vec2(319.66, 59.11));
-        });
-    });
-
+pub fn render_task(view: &mut View,ui: &mut eframe::egui::Ui, ctx: &egui::Context) {
+    view.task_background.show_size(ui, egui::vec2(319.66, 59.11));
 }
