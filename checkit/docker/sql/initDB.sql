0a261f4461188a8cb5ab29ecf67c6c25c684930c
Terrell-byte <danieltsutton@outlook.com>
2023-02-07 08:31:36 +0100

    shorten description reference for QOL

--- checkit/docker/sql/initDB.sql
+++ checkit/docker/sql/initDB.sql
@@ -2,6 +2,6 @@ CREATE TABLE checkitDB.tasks (
     id INT NOT NULL,
     date VARCHAR(255) NOT NULL,
     title VARCHAR(32) NOT NULL,
-    description TEXT NOT NULL,
+    descr TEXT NOT NULL,
     PRIMARY KEY (id, date)
 );
